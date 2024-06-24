aebe3b49fdd951c48c96cd02cea2bad918636738
Clément Bartholomé <clementoss@gmail.com>
2024-06-24 15:10:24 +0200

    feat(bdd): ajout des entités, création de la base de données et des tables

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -5,13 +5,20 @@ namespace Dot.Net.WebApi.Data
 {
     public class LocalDbContext : DbContext
     {
-        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
+        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options)
+        {
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
 
-        public DbSet<User> Users { get; set;}
+        public DbSet<User> Users { get; set; }
+        public DbSet<BidList> BidLists { get; set; }
+        public DbSet<CurvePoint> CurvePoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RuleName> RuleNames { get; set; }
+        public DbSet<Trade> Trades { get; set; }
     }
 }
