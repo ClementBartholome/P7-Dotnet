7c0cd2a8b165dcec1e21a605e4ac135faedc3162
Clément Bartholomé <clementoss@gmail.com>
2024-08-19 11:46:23 +0200

    feat(logs): logs AuthController

--- P7CreateRestApi/Controllers/AuthController.cs
+++ P7CreateRestApi/Controllers/AuthController.cs
@@ -13,43 +13,53 @@ public class AuthController : ControllerBase
     private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
     private readonly JwtService _jwtService;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, JwtService jwtService)
+    public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, JwtService jwtService, ILogger<AuthController> logger)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _jwtService = jwtService;
+        _logger = logger;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterModel model)
     {
+        _logger.LogInformation("Registering User");
         var user = new User { UserName = model.Email, FullName = model.FullName, Email = model.Email, EmailConfirmed = true };
         var result = await _userManager.CreateAsync(user, model.Password);
         if (result.Succeeded)
         {
-            return Ok(new { message = "Utilisateur enregistré avec succès" });
+            _logger.LogInformation("User registered successfully with email: {Email}", model.Email);
+            return Ok(new { message = "User registered successfully" });
         }
+        _logger.LogWarning("User registration failed for email: {Email}", model.Email);
         return BadRequest(result.Errors);
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginModel model)
     {
+        _logger.LogInformation("Login method called with email: {Email}", model.Email);
         var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
         if (result.Succeeded)
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
             var token = _jwtService.GenerateJwtToken(user);
+            _logger.LogInformation("User logged in successfully with email: {Email}", model.Email);
             return Ok(new { token });
         }
+        _logger.LogWarning("Login failed for email: {Email}", model.Email);
         return Unauthorized();
     }
 
     [HttpPost("logout")]
     public async Task<IActionResult> Logout()
     {
+        _logger.LogInformation("Logout method called");
         await _signInManager.SignOutAsync();
-        return Ok(new { message = "Utilisateur déconnecté avec succès" });
+        _logger.LogInformation("User logged out successfully");
+        return Ok(new { message = "User logged out successfully" });
     }
 }
