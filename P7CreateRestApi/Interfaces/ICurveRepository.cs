7afae88d3cb8e39ef07dfab95b8150dd78e453f4
Clément Bartholomé <clementoss@gmail.com>
2024-10-14 17:14:36 +0200

    feat(interface): ajout de l'interface ICurveRepository

---
+++ P7CreateRestApi/Interfaces/ICurveRepository.cs
@@ -0,0 +1,16 @@
+﻿using P7CreateRestApi.Models.Dto;
+using Dot.Net.WebApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P7CreateRestApi.Interfaces
+{
+    public interface ICurveRepository
+    {
+        Task<ActionResult<IEnumerable<CurvePointDto>>> GetCurves();
+        Task<CurvePointDto?> GetCurve(int id);
+        Task<CurvePoint?> UpdateCurve(int id, CurvePointDto curvePointDto);
+        Task<CurvePoint> PostCurve(CurvePointDto curvePointDto);
+        Task<bool> DeleteCurve(int id);
+        bool CurveExists(int id);
+    }
+}
